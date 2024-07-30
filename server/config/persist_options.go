1ff9bd261c72ce0cc2eef3f59a7fff1967141030
lhy1024 <admin@liudos.us>
2024-07-30 17:33:24 +0800

    update etcd to 3.5.15
    Signed-off-by: lhy1024 <admin@liudos.us>

--- server/config/persist_options.go
+++ server/config/persist_options.go
@@ -37,7 +37,7 @@ import (
 	"github.com/tikv/pd/pkg/storage/endpoint"
 	"github.com/tikv/pd/pkg/utils/etcdutil"
 	"github.com/tikv/pd/pkg/utils/typeutil"
-	"go.etcd.io/etcd/clientv3"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.uber.org/zap"
 )
 
