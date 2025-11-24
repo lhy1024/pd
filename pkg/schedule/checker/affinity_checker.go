50590d8945f0a294909d9ccac21b5923eb14ae3f
lhy1024 <admin@liudos.us>
2025-11-24 21:56:51 +0800

    add enable config
    Signed-off-by: lhy1024 <admin@liudos.us>

--- pkg/schedule/checker/affinity_checker.go
+++ pkg/schedule/checker/affinity_checker.go
@@ -69,6 +69,9 @@ func (*AffinityChecker) Name() string {
 
 // Check verifies a region's replicas according to affinity group constraints, creating an Operator if needed.
 func (c *AffinityChecker) Check(region *core.RegionInfo) []*operator.Operator {
+	if !c.conf.IsAffinitySchedulingEnabled() || c.affinityManager == nil || !c.affinityManager.IsInitialized() {
+		return nil
+	}
 	affinityCheckerCounter.Inc()
 
 	if c.IsPaused() {
